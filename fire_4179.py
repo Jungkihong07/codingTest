2b8df778e81ab80f94d64dfa055b84d2497cd12a
Jungkihong07 <137753623+Jungkihong07@users.noreply.github.com>
2024-12-10 05:46:11 +0000

    컵라면 문제 미 해결, 불 문제 해결

--- fire_4179.py
+++ fire_4179.py
@@ -25,38 +25,39 @@ for x in range(c):
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-
 def solve_problem():
     global fire_location
-    visited = [[0]*c for _ in range(r)]
-    for x,y in fire_location:
-        visited[y][x] = -1
+    visited_f = [[0]*c for _ in range(r)]
+    visited_j = [[0]*c for _ in range(r)]
     person_q = deque()
+    fire_q = deque()
+    for x,y in fire_location:
+        fire_q.append([x,y])
+        visited_f[y][x] = 1
     person_q.append((px, py))
-    visited[py][px] = 1
-    while person_q:
-        # 그 외의 모든 불들이 확산됨.
-        fire_location_ex = []
-        for x,y in fire_location:
-            frnx , frny = x,y
-            for i in range(4):
-                fnx = frnx  + dx[i]
-                fny = frny + dy[i]
-                if (0<= fnx < c) and (0<= fny < r) and maze[fny][fnx]=='.' and visited[fny][fnx] == 0:
-                    fire_location_ex.append((fnx,fny))
-                    visited[fny][fnx] = -1
-        # 확산된 불들이 다음에 더 넘어갈 수 있으므로 교체
-        fire_location= fire_location_ex
-        # 사람이 먼저 한 발짝 감.
-        prnx, prny = person_q.popleft()
-        if (prnx == 0) or (prnx == (c-1)) or (prny == 0) or (prny == (r-1)):
-            return visited[prny][prnx]
+    visited_j[py][px] = 1
+
+    # 불에 대해서 먼저 기록
+    while fire_q:
+        prnx, prny = fire_q.popleft()
         for i in range(4):
             pnx = prnx + dx[i]
             pny = prny + dy[i]
-            if (0<= pnx < c) and (0<= pny < r) and maze[pny][pnx]=='.' and visited[pny][pnx]==0:
-                person_q.append((pnx,pny))
-                visited[pny][pnx] = visited[prny][prnx] + 1
+            if (0<= pnx < c) and (0<= pny < r) and maze[pny][pnx]=='.' and not visited_f[pny][pnx]:
+                fire_q.append((pnx,pny))
+                visited_f[pny][pnx] = visited_f[prny][prnx] + 1
+    # 사람에 대해서 먼저 기록
+    while person_q:
+        prnx, prny = person_q.popleft()
+        if (prnx == 0) or (prnx == (c-1)) or (prny == 0) or (prny == (r-1)):
+            return visited_j[prny][prnx]
+        for i in range(4):
+            pnx = prnx + dx[i]
+            pny = prny + dy[i]
+            if (0<= pnx < c) and (0<= pny < r) and maze[pny][pnx]=='.' and not visited_j[pny][pnx]:
+                if (visited_f[pny][pnx] and visited_f[pny][pnx]>visited_j[prny][prnx]+1) or not visited_f[pny][pnx]:
+                    person_q.append((pnx,pny))
+                    visited_j[pny][pnx] = visited_j[prny][prnx] + 1
 
     return False       
 result = solve_problem()
