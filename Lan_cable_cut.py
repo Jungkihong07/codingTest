8b16362d9ded1d9e474eb184b6fa070694c3f977
Jungkihong07 <hnn07185@gmail.com>
2024-12-11 00:28:36 +0900

    떡볶이 떡 만들기 코드 오류 해결 + 랜선 자르기 문제 해결

--- Lan_cable_cut.py
+++ Lan_cable_cut.py
@@ -4,14 +4,17 @@ list_n = [int(input()) for _ in range(n)]
 
 def solve_problem(array, start, end):
     while start<=end:
+        mid = (end+start)//2 or 1
         total = 0
         for lan in array:
-            if lan >= end:
-                total += lan // end
+            if lan>=mid:
+                total += lan // mid
         if total < k:
-            end = (start + end) //2
+            end = mid -1
         else:
-            return end
+            result = mid
+            start = mid + 1
+    return result
 
 end = max(list_n)
 result = solve_problem(list_n,0,end)
