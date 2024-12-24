b8e9b621fbb3206c9d24f44e2545bec7f5b05b5c
Jungkihong07 <137753623+Jungkihong07@users.noreply.github.com>
2024-12-24 05:54:26 +0000

    9370 미확인 도착지 문제 해결 중

--- problem_9370.py
+++ problem_9370.py
@@ -3,25 +3,28 @@ import sys
 input = sys.stdin.readline
 INF =int(1e9)
 def dijkstra(start):
-    distance[start] =0
+    distance[start][0] =0
     q =[]
-    heapq.heappush(q,(0,start))
+    heapq.heappush(q,(0,start,False))
     while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+        dist, now, bool = heapq.heappop(q)
+        if distance[now][0] < dist:
             continue
         for i in array[now]:
             cost = dist + i[1]
-            if distance[i[0]][0] > cost:
+            if distance[i[0]][0] >= cost:
                 distance[i[0]][0] = cost
-                if i[2]:
+                if i[2] or bool:
                     distance[i[0]][1] = True
-                heapq.heappush(q,(cost,i[0]))
+                    bool = True
+                heapq.heappush(q,(cost,i[0],bool))
 
 
 
 T = int(input())
-for _ in range(T):
+result = [[] for _ in range(T)]
+
+for c in range(T):
     n,m,t = map(int,input().split())    
     s,g,h = map(int,input().split())
     array = [[] for _ in range(n+1)]
@@ -29,7 +32,7 @@ for _ in range(T):
     end= []
     for _ in range(m):
         a,b,d = map(int,input().split())
-        if a==(g or h) and b == (g or h):
+        if (a==g and b == h) or (a==h and b==g):
             array[a].append([b,d,True])
             array[b].append([a,d,True])
         else:
@@ -38,8 +41,11 @@ for _ in range(T):
     for _ in range(t):
         heapq.heappush(end,int(input()))
     dijkstra(s)
-    result = []
     for i in end:
         if distance[i][1]:
-            print(i)
+            result[c].append(i)
         
+for i in result:
+    for a in i:
+        print(a, end=' ')
+    print()
