961178320c80b3a4fb50529933a80da7deb37357
Jungkihong07 <hnn07185@gmail.com>
2024-12-30 23:47:19 +0900

    [백준] 9370번 미확인 도착지 문제 해결

--- problem_9370.py
+++ problem_9370.py
@@ -1,54 +1,48 @@
+# 백준 9370 미확인 도착지
 import heapq
 import sys
 input = sys.stdin.readline
 INF =int(1e9)
 
-def dijkstra(start):
-    distance[start][0] =0
+def dijkstra(start, distance):
+    distance[start] =0
     q =[]
-    heapq.heappush(q,(0,start,False))
+    heapq.heappush(q,(0,start))
     while q:
-        dist, now, bool = heapq.heappop(q)
-        if distance[now][0] < dist:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
             continue
         for i in array[now]:
             cost = dist + i[1]
-            if distance[i[0]][0] >= cost:
-                distance[i[0]][0] = cost
-                if i[2]:
-                    distance[i[0]][1] = True
-                    bool = True
-                if bool:
-                    distance[i[0]][1] = True
-                heapq.heappush(q,(cost,i[0],bool))
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost                
+                heapq.heappush(q,(cost,i[0]))
 
 
 
 T = int(input())
 result = [[] for _ in range(T)]
-
 for c in range(T):
     n,m,t = map(int,input().split())
     s,g,h = map(int,input().split())
     array = [[] for _ in range(n+1)]
-    distance =[[INF,False] for _ in range(n+1)]
-    end= []
+    distance =[INF for _ in range(n+1)]
+    dist_g = [INF for _ in range(n+1)]
+    dist_h = [INF for _ in range(n+1)]
+    ends= []
     for _ in range(m):
         a,b,d = map(int,input().split())
-        if (a==g and b == h) or (a==h and b==g):
-            array[a].append([b,d,True])
-            array[b].append([a,d,True])
-        else:
-            array[a].append([b,d,False])
-            array[b].append([a,d,False])
+        array[a].append([b,d])
+        array[b].append([a,d])
     for _ in range(t):
-        heapq.heappush(end,int(input()))
-    dijkstra(s)
-    for i in end:
-        if distance[i][1]:
-            result[c].append(i)
+        heapq.heappush(ends,int(input()))
+    dijkstra(s, distance)
+    dijkstra(g, dist_g)
+    dijkstra(h, dist_h)
+    for end in ends:
+        if (distance[end] == distance[g] + dist_g[h] + dist_h[end]) or (distance[end] == distance[h] + dist_h[g] + dist_g[end]):
+            heapq.heappush(result[c], end)
         
 for i in result:
-    for a in i:
-        print(a, end=' ')
-    print()
+    i.sort()
+    print(*i)
