ec3798446cfa8c5a72d40ccb8732af89da0a6c2a
Jungkihong07 <hnn07185@gmail.com>
2024-12-31 16:34:18 +0900

    5719 문제 풀이 완료.

--- problem_5719.py
+++ problem_5719.py
@@ -1,23 +1,12 @@
 import sys
 import heapq
+from collections import deque
 input = sys.stdin.readline
 INF = int(1e9)
 
-
-
-n,m = map(int,input().split())
-
-s,d = map(int,input().split())
-
-dist =[INF] * (n)
-
-array =[[] for _ in range(n)]
-
-for _ in range(m):
-    u,v,p = map(int,input().split())
-    array[u].append([v,p])
-
 def dijkstra(start):
+    dist =[INF] * (n)
+
     dist[start] = 0
     q = []
     heapq.heappush(q,(0,start))
@@ -26,7 +15,47 @@ def dijkstra(start):
         if dist[now] < distance:
             continue
         for i in array[now]:
+            if edges[now][i[0]]: continue
             cost =  distance + i[1]
             if dist[i[0]] > cost:
                 dist[i[0]] = cost
                 heapq.heappush(q,(cost, i[0]))
+    return dist
+
+def bfs():
+    q = deque()
+    q.append(d)
+    
+    while q:
+        current = q.popleft()
+
+        if current == s:
+            continue
+        for node, cost in array_inv[current]:
+            if dist[node] + cost == dist[current] and not edges[node][current]:
+                edges[node][current] = True
+                q.append(node)
+
+result = []
+while 1:
+    n,m = map(int,input().split())
+
+    if not (n and m):
+        break
+    s,d = map(int,input().split())
+
+    array =[[] for _ in range(n)]
+    array_inv =[[] for _ in range(n)]
+    edges = [ [False]*(n+1) for _ in range(n+1)]
+    for _ in range(m):
+        u,v,p = map(int,input().split())
+        array[u].append([v,p])
+        array_inv[v].append([u,p])
+
+    dist = dijkstra(s)
+    bfs()
+    dist = dijkstra(s)
+    result.append(-1 if dist[d] == INF else dist[d])
+
+for a in result:
+    print(a)
