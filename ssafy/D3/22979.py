02525359bc8e52ccbb4d427bff30a516e24e0fec
Jungkihong07 <hnn07185@gmail.com>
2025-05-07 21:05:42 +0900

    20955, 22979 풀이 완료

--- ssafy/D3/22979.py
+++ ssafy/D3/22979.py
@@ -1,9 +1,20 @@
-def operate(s:list, )
+def operate(s: list, x_list: list):
+    for x in x_list:
+        x %= len(s)
+        if x > 0:
+            for _ in range(x):
+                s = s[1:] + s[0]
+        elif x < 0:
+            for _ in range(abs(x)):
+                s = s[-1] + s[:-1]
+        else:
+            pass
+    return s
 
 
 T = int(input())
 for i in range(1, T + 1):
-    s = list(map(str, input().strip()))
+    s = input().strip()
     k = int(input())
     _list = list(map(int, input().split()))
-
+    print(operate(s, _list))
