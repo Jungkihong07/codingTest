71dc2209eb98fb3107452609b411b55c29b96d8c
Jungkihong07 <137753623+Jungkihong07@users.noreply.github.com>
2025-05-22 08:57:44 +0000

    문제 해결 평가해보기

--- ssafy/D3/1860.py
+++ ssafy/D3/1860.py
@@ -1,4 +1,4 @@
-def solve(people, make_time, bread, come_time):
+def solve(make_time, bread, come_time):
     current_bread = 0
     total_time = sum(come_time)
     total_bread = []
@@ -9,10 +9,15 @@ def solve(people, make_time, bread, come_time):
     time = 0
     for i in come_time:
         time += i
-        total_bread[time]
+        for j in range(time, total_time):
+            total_bread[j] -= 1
+            if total_bread[j] < 0:
+                return False
+    return True
 
 
 T = int(input())
 for t in range(1, T + 1):
     people, make_time, bread = map(int, input().split())
     come_time = list(map(int, input()))
+    print(f"#{t} {solve(make_time, bread, come_time)}")
