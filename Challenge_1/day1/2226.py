db9c7af97be1d91d459568092922be6c3652ee61
Jungkihong07 <hnn07185@gmail.com>
2025-06-16 00:22:10 +0900

    2226 해결 완료

--- Challenge_1/day1/2226.py
+++ Challenge_1/day1/2226.py
@@ -1,19 +1,11 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
 
-bisect = [[1001]]
-
-ch = 1
-
-for i in range(2, n):
-    reverse_temp = []
-    for ch in bisect:
-        if ch == [1001]:
-            reverse_temp.append([0110])
-        else:
-            reverse_temp.append([1001])
-    bisect = reverse_temp + bisect
-    print(*bisect)
+prev = 0
+current = 1
+for i in range(2, n + 1):
+    if i % 2 == 0:
+        current = prev * 2 + 1
+    else:
+        current = prev * 2 - 1
+    prev = current
+print(current if n != 1 else 0)
