7d329acf779a9798050577e49872da25e73fb413
Jungkihong07 <hnn07185@gmail.com>
2025-06-17 10:51:42 +0900

    더 좋은 방식으로 풀이 완료

--- Challenge_1/day3/1978.py
+++ Challenge_1/day3/1978.py
@@ -1,13 +1,12 @@
 n = int(input())
 nums = list(map(int, input().split()))
-decimal_list = [0, 0] + [1] * 1000
+MAX = max(nums) + 1
+decimal_list = [0, 0] + [1] * MAX
 
-for i in range(2, 101):
-    if decimal_list[i] == 1:
-        index = i + i
-        while index < len(decimal_list):
-            decimal_list[index] = 0
-            index += i
+for i in range(2, int(MAX**0.5) + 1):
+    if decimal_list[i]:
+        for j in range(i * i, MAX, i):
+            decimal_list[j] = 0
 
 result = 0
 for num in nums:
