87232453a9ffaa0afb3d2b2cb291f202ef240728
Jungkihong07 <hnn07185@gmail.com>
2025-06-17 10:23:41 +0900

    N 번째 큰수 문제 풀이 완료

---
+++ Challenge_1/day3/2075.py
@@ -0,0 +1,14 @@
+import heapq
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    row = list(map(int, input().split()))
+    for num in row:
+        if len(heap) < n:
+            heapq.heappush(heap, num)
+        else:
+            if num > heap[0]:
+                heapq.heappushpop(heap, num)
+print(heap[0])
