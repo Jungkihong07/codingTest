05edd7399767b726c3aecfaba4f38b8eb8ccc64e
Jungkihong07 <hnn07185@gmail.com>
2025-06-25 23:13:18 +0900

    1806 문제 해결

--- Challenge_1/day9/1806.py
+++ Challenge_1/day9/1806.py
@@ -1,13 +1,19 @@
+import sys
+
 n, s = map(int, input().split())
 n_list = list(map(int, input().split()))
-
-n_list.sort()
-
-
-min = 1000000000
-sum_list = n_list
-for i in range(2, n + 1):
-    compare_list = []
-    for j in range(n):
-        max_item = max(sum_list[j - i : j + 1])
-        compare_list.append()
+start = 0
+end = 0
+min_len = sys.maxsize
+sum_num = 0
+while 1:
+    if s <= sum_num:
+        min_len = min(min_len, end - start)
+        sum_num -= n_list[start]
+        start += 1
+    elif end == n:
+        break
+    else:
+        sum_num += n_list[end]
+        end += 1
+print(min_len if min_len < sys.maxsize else 0)
