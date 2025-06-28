f06080bf7c0f5938064a46d6f5fe57f8684390ba
Jungkihong07 <hnn07185@gmail.com>
2025-06-29 00:21:44 +0900

    비 효율적인 방식으로 구현. 추가 리펙토링 고려 (1916 문제)

--- Challenge_1/day11/1916.py
+++ Challenge_1/day11/1916.py
@@ -1,33 +1,33 @@
 import sys
-from collections import deque
-
+import heapq
 
 # 최소 비용 구하기
-n = int(input())
-m = int(input())
+n = int(input())  # 도시의 갯수
+m = int(input())  # 간선의 갯수
 weigth_board = [
     [0 if i == j else sys.maxsize for i in range(n + 1)] for j in range(n + 1)
-]
+]  # 도시 별 이동 가중치 기록
+
+# 도시 별 이동 가중치 기록 2
 for _ in range(m):
     start, end, weight = map(int, input().split())
     weigth_board[start][end] = min(weigth_board[start][end], weight)
+# 출발지와 도착지 입력
 start, end = map(int, input().split())
-
-q = deque()
-q.append(start)
-start_board = [sys.maxsize] * (n + 1)
+# 순서 대로 입력 (가중치, 출발지)
+heap = []
+heapq.heappush(heap, (0, start))
+# 출발지 기준, 거리 계산
+dist = [sys.maxsize] * (n + 1)
+dist[start] = 0
 visited = [False] * (n + 1)
 visited[start] = True
-start_board[start] = 0
-
-while q:
-    next_node = q.popleft()
+while heap:
+    cost, now = heapq.heappop(heap)
     for i in range(1, n + 1):
-        if weigth_board[next_node][i] != sys.maxsize and weigth_board[next_node][i]:
-            start_board[i] = min(
-                start_board[next_node] + weigth_board[next_node][i], start_board[i]
-            )
+        if weigth_board[now][i] != sys.maxsize:
+            dist[i] = min(dist[now] + weigth_board[now][i], dist[i])
             if not visited[i]:
                 visited[i] = True
-                q.append(i)
-print(start_board[end])
+                heapq.heappush(heap, (dist[i], i))
+print(dist[end])
